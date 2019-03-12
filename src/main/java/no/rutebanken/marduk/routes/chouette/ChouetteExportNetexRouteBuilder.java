7089d33098163597f89b933f145f188cc2636924
Grégoire Fora <gfora@okina.fr>
2019-03-12 09:04:32 +0100

    Deploy for prod pigma export

--- src/main/java/no/rutebanken/marduk/routes/chouette/ChouetteExportNetexRouteBuilder.java
+++ src/main/java/no/rutebanken/marduk/routes/chouette/ChouetteExportNetexRouteBuilder.java
@@ -94,7 +94,7 @@ public class ChouetteExportNetexRouteBuilder extends AbstractChouetteRouteBuilde
 
                     .setHeader(BLOBSTORE_MAKE_BLOB_PUBLIC, constant(publicPublication))
                     .setHeader(FILE_HANDLE, simple(BLOBSTORE_PATH_OUTBOUND + "netex/${header." + CHOUETTE_REFERENTIAL + "}-" + Constants.CURRENT_AGGREGATED_NETEX_FILENAME))
-                    .to("direct:uploadPigma")
+//                    .to("direct:uploadPigma")
                     .to("direct:uploadBlob")
                     .process(e -> JobEvent.providerJobBuilder(e).timetableAction(JobEvent.TimetableAction.EXPORT_NETEX).state(JobEvent.State.OK).build())
                     .to("direct:updateStatus")
