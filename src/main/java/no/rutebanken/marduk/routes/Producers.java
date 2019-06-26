cc18508daf5471d08447ff825c05a31e72582a4a
Grégoire Fora <gfora@okina.fr>
2019-06-26 17:02:10 +0200

    Change metadata file

--- src/main/java/no/rutebanken/marduk/routes/Producers.java
+++ src/main/java/no/rutebanken/marduk/routes/Producers.java
@@ -67,6 +67,111 @@ public class Producers {
     }
 
 
+    public Map<String, String> producersTransportTypeList(){
+        Map<String, String> producersTransportType = new HashMap<>();
+        //AOM
+        producersTransportType.put("bme","urbain");
+        producersTransportType.put("bri","urbain");
+        producersTransportType.put("chl","urbain");
+        producersTransportType.put("ang","urbain");
+        producersTransportType.put("cog","urbain");
+        producersTransportType.put("lro","urbain");
+        producersTransportType.put("lim","urbain");
+        producersTransportType.put("nio","urbain");
+        producersTransportType.put("pau","urbain");
+        producersTransportType.put("roc","urbain");
+        producersTransportType.put("car","urbain");
+        producersTransportType.put("tut","urbain");
+        producersTransportType.put("cou","urbain");
+        producersTransportType.put("per","urbain");
+        producersTransportType.put("vit","urbain");
+        producersTransportType.put("yeg","urbain");
+        producersTransportType.put("ber","urbain");
+        producersTransportType.put("age","urbain");
+        producersTransportType.put("vdg","urbain");
+        producersTransportType.put("bda","urbain");
+        producersTransportType.put("vil","urbain");
+        producersTransportType.put("lib","urbain");
+        producersTransportType.put("mdm","urbain");
+        producersTransportType.put("pba","urbain");
+        producersTransportType.put("gue","urbain");
+        producersTransportType.put("bbr","urbain");
+        producersTransportType.put("ole","fluvial");
+        producersTransportType.put("sai","urbain");
 
+        //Sites territorialisés
+        producersTransportType.put("cha","interurbain");
+        producersTransportType.put("cma","interurbain");
+        producersTransportType.put("cor","interurbain");
+        producersTransportType.put("cre","interurbain");
+        producersTransportType.put("dse","interurbain");
+        producersTransportType.put("dor","interurbain");
+        producersTransportType.put("gir","interurbain");
+        producersTransportType.put("hvi","interurbain");
+        producersTransportType.put("lan","interurbain");
+        producersTransportType.put("lga","interurbain");
+        producersTransportType.put("pat","interurbain");
+        producersTransportType.put("vie","interurbain");
+        producersTransportType.put("bac","fluvial");
+        producersTransportType.put("fai","fluvial");
+        producersTransportType.put("snc","ferroviaire");
+
+        return producersTransportType;
+    }
+
+
+
+    public Map<String, String> producersGranulariteList(){
+        Map<String, String> producersGranularite = new HashMap<>();
+        //AOM
+        producersGranularite.put("bme","ECPI");
+        producersGranularite.put("bri","ECPI");
+        producersGranularite.put("chl","ECPI");
+        producersGranularite.put("ang","ECPI");
+        producersGranularite.put("cog","ECPI");
+        producersGranularite.put("lro","ECPI");
+        producersGranularite.put("lim","ECPI");
+        producersGranularite.put("nio","ECPI");
+        producersGranularite.put("pau","ECPI");
+        producersGranularite.put("roc","ECPI");
+        producersGranularite.put("car","ECPI");
+        producersGranularite.put("tut","ECPI");
+        producersGranularite.put("cou","ECPI");
+        producersGranularite.put("per","ECPI");
+        producersGranularite.put("vit","ECPI");
+        producersGranularite.put("yeg","ECPI");
+        producersGranularite.put("ber","ECPI");
+        producersGranularite.put("age","ECPI");
+        producersGranularite.put("vdg","ECPI");
+        producersGranularite.put("bda","ECPI");
+        producersGranularite.put("vil","ECPI");
+        producersGranularite.put("lib","ECPI");
+        producersGranularite.put("mdm","ECPI");
+        producersGranularite.put("pba","ECPI");
+        producersGranularite.put("gue","ECPI");
+        producersGranularite.put("bbr","ECPI");
+        producersGranularite.put("ole","ECPI");
+        producersGranularite.put("sai","ECPI");
+
+
+        //Sites territorialisés
+        producersGranularite.put("cha","Département");
+        producersGranularite.put("cma","Département");
+        producersGranularite.put("cor","Département");
+        producersGranularite.put("cre","Département");
+        producersGranularite.put("dse","Département");
+        producersGranularite.put("dor","Département");
+        producersGranularite.put("gir","Département");
+        producersGranularite.put("hvi","Département");
+        producersGranularite.put("lan","Département");
+        producersGranularite.put("lga","Département");
+        producersGranularite.put("pat","Département");
+        producersGranularite.put("vie","Département");
+        producersGranularite.put("bac","Département");
+        producersGranularite.put("fai","Département");
+        producersGranularite.put("snc","Région");
+
+        return producersGranularite;
+    }
 
 }
---
+++ src/main/java/no/rutebanken/marduk/routes/pigma/MetadataFile.java
@@ -0,0 +1,95 @@
+package no.rutebanken.marduk.routes.pigma;
+
+import no.rutebanken.marduk.Utils.ExportCsv;
+import no.rutebanken.marduk.domain.BlobStoreFiles;
+import no.rutebanken.marduk.routes.Producers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.util.ArrayList;
+
+@Component
+public class MetadataFile {
+
+    @Autowired
+    private Producers producers;
+
+    @Autowired
+    private ExportCsv exportCsv;
+
+
+    private static final String[] FILE_HEADER_PRINTER = new String[]{"Titre", "Description", "Mots clés", "Frequence de mise à jour", "Licence", "Granularité", "Contact", "Titre du fichier", "Nom fichier GTFS", "Nom fichier Netex"};
+
+    private static final String[] FILE_ROW_PRINTER_GENERAL = new String[]{
+            "Arrêts, horaires et parcours théoriques des réseaux de transport public des membres du syndicat Nouvelle-Aquitaine Mobilités.",
+            "Ce jeu de données contient la liste des arrêts, des horaires et des parcours théoriques des réseaux de transport public des membres du syndicat Nouvelle-Aquitaine Mobilités.",
+            "#NETEX#GTFS#données-ouvertes",
+            "hebdomadaire",
+            "Région",
+            "contact@nouvelle-aquitaine-mobilites.fr",
+            "Open Data Commons Open Database License (OdbL)",
+            "RESEAUX-NOUVELLE-AQUITAINE-MOBILITES",
+            "naq-aggregated-gtfs.zip",
+            "naq-aggregated-netex.zip"
+    };
+
+    private static final String[] FILE_ROW_PRINTER_PRODUCER = new String[]{
+            "Arrêts horaires et parcours théoriques des bus du réseau des transports publics",
+            "Ce jeu de données contient la liste des arrêts, des horaires et des parcours théoriques du réseau des transports publics",
+            "#NETEX#GTFS#données-ouvertes",
+            "hebdomadaire",
+            "Open Data Commons Open Database License (OdbL)",
+            "Réseau ",
+            "",
+            ""
+    };
+
+    private static final String[] FILE_ROW_PRINTER_STOPS = new String[]{
+            "Arrêts des réseaux de transport public des membres du syndicat Nouvelle-Aquitaine Mobilités.",
+            "Ce jeu de données contient la liste des arrêts des réseaux de transport public des membres du syndicat Nouvelle-Aquitaine Mobilités.",
+            "#NETEX#GTFS#données-ouvertes",
+            "hebdomadaire",
+            "Open Data Commons Open Database License (OdbL)",
+            "",
+            "contact@nouvelle-aquitaine-mobilites.fr",
+            "RESEAUX-NOUVELLE-AQUITAINE-MOBILITES",
+            "",
+            "naq-stops-netex.zip"
+    };
+
+    public File createMetadataFile(String nameFile, ArrayList<BlobStoreFiles.File> listBlobStoreFiles){
+        String[] data;
+        ArrayList<String[]> rows = new ArrayList<>();
+
+        for(BlobStoreFiles.File blobStoreFile : listBlobStoreFiles){
+            if(!nameFile.equals("CurrentAndFuture_latest.zip") && blobStoreFile.getReferential() == null){
+                data = FILE_ROW_PRINTER_GENERAL;
+                rows.add(data);
+            }
+            else if (nameFile.equals("CurrentAndFuture_latest.zip")){
+                data = FILE_ROW_PRINTER_STOPS;
+                rows.add(data);
+            }
+            else{
+                String[] prefixParts = blobStoreFile.getReferential().split("_");
+                String producerName = producers.producersListName().get(prefixParts[1]);
+                String type = producers.producersTransportTypeList().get(prefixParts[1]);
+
+                String title = FILE_ROW_PRINTER_PRODUCER[0] + type + " de " + producerName;
+                String description = FILE_ROW_PRINTER_PRODUCER[1] + type + " de " + producerName;
+                String granularite = producers.producersGranulariteList().get(prefixParts[1]);
+                String contact = "contact@nouvelle-aquitaine-mobilites.fr";
+                String titleFile = FILE_ROW_PRINTER_PRODUCER[5] + producerName;
+                String titleGtfs = blobStoreFile.getReferential() + "-aggregated-gtfs.zip";
+                String titleNetex = blobStoreFile.getReferential() + "-aggregated-netex.zip";
+
+                data = new String[]{title, description, FILE_ROW_PRINTER_PRODUCER[2], FILE_ROW_PRINTER_PRODUCER[3], FILE_ROW_PRINTER_PRODUCER[4], granularite, contact, titleFile, titleGtfs, titleNetex};
+                rows.add(data);
+            }
+        }
+
+        return exportCsv.createCSVFile(nameFile, FILE_HEADER_PRINTER, rows);
+    }
+
+}
