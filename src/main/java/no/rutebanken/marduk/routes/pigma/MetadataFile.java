f9de886acca757f8c134063b4a4c08342e90cdb7
Grégoire Fora <gfora@okina.fr>
2019-06-27 11:40:03 +0200

    Change metadata file (3)

--- src/main/java/no/rutebanken/marduk/routes/pigma/MetadataFile.java
+++ src/main/java/no/rutebanken/marduk/routes/pigma/MetadataFile.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class MetadataFile {
@@ -23,7 +25,7 @@ public class MetadataFile {
 
     private static final String[] FILE_ROW_PRINTER_GENERAL = new String[]{
             "Arrêts horaires et parcours théoriques des réseaux de transport public des membres du syndicat Nouvelle-Aquitaine Mobilités.",
-            "Ce jeu de données contient la liste des arrêts, des horaires et des parcours théoriques des réseaux de transport public des membres du syndicat Nouvelle-Aquitaine Mobilités.",
+            "Ce jeu de données contient la liste des arrêts des horaires et des parcours théoriques des réseaux de transport public des membres du syndicat Nouvelle-Aquitaine Mobilités.",
             "#NETEX#GTFS#données-ouvertes",
             "hebdomadaire",
             "Région",
@@ -51,45 +53,52 @@ public class MetadataFile {
             "#NETEX#GTFS#données-ouvertes",
             "hebdomadaire",
             "Open Data Commons Open Database License (OdbL)",
-            "",
+            "Région",
             "contact@nouvelle-aquitaine-mobilites.fr",
             "RESEAUX-NOUVELLE-AQUITAINE-MOBILITES",
             "",
             "naq-stops-netex.zip"
     };
 
-    public File createMetadataFile(String nameFile, ArrayList<BlobStoreFiles.File> listBlobStoreFiles){
+    public File createMetadataFile(String nameFile, ArrayList<BlobStoreFiles.File> listBlobStoreFiles) {
         String[] data;
         ArrayList<String[]> rows = new ArrayList<>();
 
-        for(BlobStoreFiles.File blobStoreFile : listBlobStoreFiles){
-            if(!blobStoreFile.getFileNameOnly().equals("CurrentAndFuture_latest.zip") && blobStoreFile.getReferential() == null){
+        List<String> listPrefix =
+                listBlobStoreFiles
+                        .stream()
+                        .filter(file -> file.getReferential() != null)
+                        .map(BlobStoreFiles.File::getReferential)
+                        .distinct()
+                        .collect(Collectors.toList());
+
+        for (BlobStoreFiles.File blobStoreFile : listBlobStoreFiles) {
+            if (!blobStoreFile.getFileNameOnly().equals("CurrentAndFuture_latest.zip") && blobStoreFile.getReferential() == null) {
                 data = FILE_ROW_PRINTER_GENERAL;
                 rows.add(data);
-            }
-            else if (blobStoreFile.getFileNameOnly().equals("CurrentAndFuture_latest.zip")){
+            } else if (blobStoreFile.getFileNameOnly().equals("CurrentAndFuture_latest.zip")) {
                 data = FILE_ROW_PRINTER_STOPS;
                 rows.add(data);
             }
-            else{
-                String[] prefixParts = blobStoreFile.getReferential().split("_");
-                String producerName = producers.producersListName().get(prefixParts[1]);
-                String type = producers.producersTransportTypeList().get(prefixParts[1]);
-
-                String title = FILE_ROW_PRINTER_PRODUCER[0] + " " + type + " de " + producerName;
-                String description = FILE_ROW_PRINTER_PRODUCER[1] + " " + type + " de " + producerName;
-                String granularite = producers.producersGranulariteList().get(prefixParts[1]);
-                String contact = "contact@nouvelle-aquitaine-mobilites.fr";
-                String titleFile = FILE_ROW_PRINTER_PRODUCER[5] + producerName;
-                String titleGtfs = blobStoreFile.getReferential() + "-aggregated-gtfs.zip";
-                String titleNetex = blobStoreFile.getReferential() + "-aggregated-netex.zip";
-
-                data = new String[]{title, description, FILE_ROW_PRINTER_PRODUCER[2], FILE_ROW_PRINTER_PRODUCER[3], FILE_ROW_PRINTER_PRODUCER[4], granularite, contact, titleFile, titleGtfs, titleNetex};
-                rows.add(data);
-            }
         }
+        for (String prefix : listPrefix) {
+            String[] prefixParts = prefix.split("_");
+            String producerName = producers.producersListName().get(prefixParts[1]);
+            String type = producers.producersTransportTypeList().get(prefixParts[1]);
 
-        return exportCsv.createCSVFile(nameFile, FILE_HEADER_PRINTER, rows);
+            String title = FILE_ROW_PRINTER_PRODUCER[0] + " " + type + " de " + producerName;
+            String description = FILE_ROW_PRINTER_PRODUCER[1] + " " + type + " de " + producerName;
+            String granularite = producers.producersGranulariteList().get(prefixParts[1]);
+            String contact = "contact@nouvelle-aquitaine-mobilites.fr";
+            String titleFile = FILE_ROW_PRINTER_PRODUCER[5] + producerName;
+            String titleGtfs = prefix + "-aggregated-gtfs.zip";
+            String titleNetex = prefix + "-aggregated-netex.zip";
+
+            data = new String[]{title, description, FILE_ROW_PRINTER_PRODUCER[2], FILE_ROW_PRINTER_PRODUCER[3], FILE_ROW_PRINTER_PRODUCER[4], granularite, contact, titleFile, titleGtfs, titleNetex};
+            rows.add(data);
     }
+
+        return exportCsv.createCSVFile(nameFile,FILE_HEADER_PRINTER,rows);
+}
 
 }
