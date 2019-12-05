d56c56f9da528c3269cc65febc2575665bc28060
Grégoire Fora <gfora@okina.fr>
2019-12-05 11:27:30 +0100

    Revert "Temporarily disable GTFS export"
    This reverts commit dff3a112

--- src/main/java/no/rutebanken/marduk/routes/chouette/ChouetteExportNetexRouteBuilder.java
+++ src/main/java/no/rutebanken/marduk/routes/chouette/ChouetteExportNetexRouteBuilder.java
@@ -102,8 +102,7 @@ public class ChouetteExportNetexRouteBuilder extends AbstractChouetteRouteBuilde
                     .setBody(constant(null))
                     .process(e -> JobEvent.providerJobBuilder(e).timetableAction(JobEvent.TimetableAction.BUILD_GRAPH).state(JobEvent.State.PENDING).build())
                     .to("direct:exportMergedNetex")
-//                TODO désactivation temporaire de l'export GTFS
-//                    .to("activemq:queue:ChouetteExportGtfsQueue")
+                    .to("activemq:queue:ChouetteExportGtfsQueue")
                     .endChoice()
                 .when(simple("${header.action_report_result} == 'NOK'"))
                     .log(LoggingLevel.WARN, correlation() + "Netex export failed")
