98052f6bf2977a7f2c3e2106cb4d60169db84aaf
mmerlin <mmerlin@okina.fr>
2020-01-08 11:52:27 +0100

    Fix import details: User Description

--- src/main/java/no/rutebanken/marduk/routes/file/FileInformations.java
+++ src/main/java/no/rutebanken/marduk/routes/file/FileInformations.java
@@ -12,13 +12,9 @@ import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
-
-import static no.rutebanken.marduk.Constants.DESCRIPTION;
-import static no.rutebanken.marduk.Constants.USER;
 
 public class FileInformations {
 
@@ -28,39 +24,6 @@ public class FileInformations {
             bytes = IOUtils.toByteArray(e.getIn().getBody(InputStream.class));
         } catch (Exception ex) {
             throw new MardukException("Failed to parse multipart content: " + ex.getMessage());
-        }
-
-        String informations = "";
-
-        try {
-            InputStream inputStream = new ByteArrayInputStream(bytes);
-            informations = IOUtils.toString(inputStream, "UTF-8");
-        } catch (UnsupportedEncodingException ex) {
-            ex.printStackTrace();
-        }
-
-        int indexOfUser = informations.lastIndexOf("Content-Disposition: form-data; name=\"user\"");
-        if (indexOfUser != -1) {
-            String user = informations.substring(indexOfUser);
-            indexOfUser = user.indexOf("------WebKitFormBoundary");
-            if (indexOfUser != -1) {
-                user = user.substring(0, indexOfUser);
-                user = user.substring(user.lastIndexOf('"') + 1);
-                user = user.trim();
-                e.getIn().setHeader(USER, user);
-            }
-        }
-
-        int indexOfDescription = informations.lastIndexOf("Content-Disposition: form-data; name=\"description\"");
-        if (indexOfDescription != -1) {
-            String description = informations.substring(indexOfDescription);
-            indexOfDescription = description.indexOf("------WebKitFormBoundary");
-            if (indexOfDescription != -1) {
-                description = description.substring(0, indexOfDescription);
-                description = description.substring(description.lastIndexOf('"') + 1);
-                description = description.trim();
-                e.getIn().setHeader(DESCRIPTION, description);
-            }
         }
 
         convertBodyToFileItems(e, bytes);
