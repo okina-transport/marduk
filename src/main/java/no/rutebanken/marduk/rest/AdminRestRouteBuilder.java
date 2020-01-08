1cc4dd219a482d0747c91bf7fbaf21a1bc868c4c
mmerlin <mmerlin@okina.fr>
2020-01-08 11:51:16 +0100

    CORS: Ajout de RutebankenUser et RutebankenDescription

--- src/main/java/no/rutebanken/marduk/rest/AdminRestRouteBuilder.java
+++ src/main/java/no/rutebanken/marduk/rest/AdminRestRouteBuilder.java
@@ -75,7 +75,7 @@ public class AdminRestRouteBuilder extends BaseRouteBuilder {
 
         RestPropertyDefinition corsAllowedHeaders = new RestPropertyDefinition();
         corsAllowedHeaders.setKey("Access-Control-Allow-Headers");
-        corsAllowedHeaders.setValue("Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization, x-okina-referential, User, Description");
+        corsAllowedHeaders.setValue("Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization, x-okina-referential, RutebankenUser, RutebankenDescription");
 
         restConfiguration().setCorsHeaders(Collections.singletonList(corsAllowedHeaders));
 
