6d6b464b409670da7cf9b48be1892369419eba7e
pferreira <ferreirasilvapaul@gmail.com>
2020-07-07 10:22:48 +0200

    feat/7438_config_fonctionnelle: retrieve export templates from okina back

--- src/main/java/no/rutebanken/marduk/routes/chouette/ChouetteExportAllRouteBuilder.java
+++ src/main/java/no/rutebanken/marduk/routes/chouette/ChouetteExportAllRouteBuilder.java
@@ -1,10 +1,14 @@
 package no.rutebanken.marduk.routes.chouette;
 
 import no.rutebanken.marduk.Constants;
+import no.rutebanken.marduk.domain.ExportTemplate;
+import no.rutebanken.marduk.repository.ExportTemplateDAO;
 import org.apache.camel.LoggingLevel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.UUID;
 
 import static no.rutebanken.marduk.Constants.PROVIDER_ID;
@@ -12,9 +16,8 @@ import static no.rutebanken.marduk.Constants.PROVIDER_ID;
 @Component
 public class ChouetteExportAllRouteBuilder extends AbstractChouetteRouteBuilder {
 
-
-    @Value("${chouette.url}")
-    private String chouetteUrl;
+    @Autowired
+    private ExportTemplateDAO exportTemplateDAO;
 
     @Override
     public void configure() throws Exception {
@@ -27,6 +30,8 @@ public class ChouetteExportAllRouteBuilder extends AbstractChouetteRouteBuilder 
                 // Add correlation id only if missing
                 e.getIn().setHeader(Constants.CORRELATION_ID, e.getIn().getHeader(Constants.CORRELATION_ID, UUID.randomUUID().toString()));
                 e.getIn().removeHeader(Constants.CHOUETTE_JOB_ID);
+                List<ExportTemplate> exports = exportTemplateDAO.getAll();
+                log.info("Found export templates " + exports.size());
             })
             .to("activemq:queue:ChouettePollStatusQueue")
             .routeId("chouette-send-export-all-job");
