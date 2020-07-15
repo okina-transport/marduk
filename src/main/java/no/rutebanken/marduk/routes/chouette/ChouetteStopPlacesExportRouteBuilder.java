6030acdece406573cbfbcda20e0725e54a089ef5
pferreira <ferreirasilvapaul@gmail.com>
2020-07-15 11:07:13 +0200

    feat/7438_config_fonctionnelle: stop places export new route

--- src/main/java/no/rutebanken/marduk/routes/chouette/ChouetteStopPlacesExportRouteBuilder.java
+++ src/main/java/no/rutebanken/marduk/routes/chouette/ChouetteStopPlacesExportRouteBuilder.java
@@ -2,6 +2,7 @@ package no.rutebanken.marduk.routes.chouette;
 
 import org.apache.camel.LoggingLevel;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import static no.rutebanken.marduk.Constants.PROVIDER_ID;
 
@@ -9,6 +10,7 @@ import static no.rutebanken.marduk.Constants.PROVIDER_ID;
 /**
  * Exports stop places for specific provider
  */
+@Component
 public class ChouetteStopPlacesExportRouteBuilder extends AbstractChouetteRouteBuilder {
 
     @Value("${stop-places-export.api.url}")
@@ -21,6 +23,9 @@ public class ChouetteStopPlacesExportRouteBuilder extends AbstractChouetteRouteB
         from("direct:chouetteStopPlacesExport").streamCaching()
                 .transacted()
                 .log(LoggingLevel.INFO, getClass().getName(), "Starting Chouette export stop places for provider with id ${header." + PROVIDER_ID + "}")
+                .process(e -> {
+                    log.info("chouetteStopPlacesExport : processing export ....");
+                })
                 .toD(stopPlacesExportUrl + "?providerId= ${header." + PROVIDER_ID + "}")
                 .routeId("chouette-stop-places-export-job");
     }
