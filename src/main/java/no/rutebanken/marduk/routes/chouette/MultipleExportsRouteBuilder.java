e72df56423798bc9b70f41d34dd13cb3a768f336
pferreira <ferreirasilvapaul@gmail.com>
2020-08-08 09:58:50 +0200

    feat/7650_validation_exports_back: dedicated validation export process route

---
+++ src/main/java/no/rutebanken/marduk/routes/chouette/MultipleExportsRouteBuilder.java
@@ -0,0 +1,23 @@
+package no.rutebanken.marduk.routes.chouette;
+
+import org.apache.camel.LoggingLevel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import static no.rutebanken.marduk.Constants.PROVIDER_ID;
+
+@Component
+public class MultipleExportsRouteBuilder extends AbstractChouetteRouteBuilder {
+    @Autowired
+    private MultipleExportProcessor multipleExportProcessor;
+
+    @Override
+    public void configure() throws Exception {
+        super.configure();
+
+        from("direct:multipleExports").streamCaching()
+                .log(LoggingLevel.INFO, getClass().getName(), "Starting multiple exports route for provider with id ${header." + PROVIDER_ID + "}")
+                .process(multipleExportProcessor)
+                .routeId("marduk-multiple-exports-job");
+    }
+}
