b15d30a4bfe14db1962678662c90ebe658030c70
rlambert <rlambert@extia.ro>
2024-01-10 17:17:41 +0100

    feat: 19244 archive trips on stop_times.txt model

--- src/main/java/no/rutebanken/marduk/routes/file/GtfsFilesArchiver.java
+++ src/main/java/no/rutebanken/marduk/routes/file/GtfsFilesArchiver.java
@@ -15,9 +15,9 @@ import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
 @Component
-public class StopTimesArchiver {
+public class GtfsFilesArchiver {
 
-    private static Logger logger = LoggerFactory.getLogger(StopTimesArchiver.class);
+    private static Logger logger = LoggerFactory.getLogger(GtfsFilesArchiver.class);
 
     @Value("${chouette.storage.path:/srv/docker-data/data/chouette}")
     private String chouetteStoragePath;
@@ -41,7 +41,7 @@ public class StopTimesArchiver {
             logger.info("Archiving stop_times.txt from zip : " + inputFile.getAbsolutePath());
             ZipFileUtils.unzipFile(targetStream,newDir.getAbsolutePath());
             File originalFile = new File(newDir, "stop_times.txt");
-            File archivedFile = new File(getDestinationDir(organisation), getDestinationFileName());
+            File archivedFile = new File(getDestinationDir(organisation, "stop_times"), getDestinationFileName("stop_times_"));
             archivedFile.mkdirs();
             logger.info("to file : " + archivedFile.getAbsolutePath());
             Files.copy(originalFile.toPath(), archivedFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
@@ -49,6 +49,36 @@ public class StopTimesArchiver {
 
         }catch (Exception e){
             logger.error("Error while archiving stop times", e);
+        }
+
+    }
+
+    /**
+     * Read a gtfs, unzip it, and saves stop_times.txt into chouetteDir/mobiiti_technique/stop_times/"organisationName" directory
+     * @param inputFile
+     *  the gtfs to read
+     * @param organisation
+     *  the organisation name
+     */
+    public void archiveTrips(File inputFile, String organisation)  {
+
+        try{
+            InputStream targetStream = new FileInputStream(inputFile);
+            String tmpDir = UUID.randomUUID().toString();
+            File newDir = new File("/tmp", tmpDir);
+            newDir.mkdirs();
+
+            logger.info("Archiving trips.txt from zip : " + inputFile.getAbsolutePath());
+            ZipFileUtils.unzipFile(targetStream,newDir.getAbsolutePath());
+            File originalFile = new File(newDir, "trips.txt");
+            File archivedFile = new File(getDestinationDir(organisation, "trips"), getDestinationFileName("trips_"));
+            archivedFile.mkdirs();
+            logger.info("to file : " + archivedFile.getAbsolutePath());
+            Files.copy(originalFile.toPath(), archivedFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            ZipFileUtils.deleteDirectory(newDir);
+
+        }catch (Exception e){
+            logger.error("Error while archiving trips", e);
         }
 
     }
@@ -64,14 +94,24 @@ public class StopTimesArchiver {
     }
 
     /**
+     * Delete the organisation directoty (in mobiiti_technique/trips) and all its files
+     * @param organisationName
+     *  the organisation for which the directory must be deleted
+     */
+    public void cleanOrganisationTrips(String organisationName){
+        File organisationDirectory = new File( chouetteStoragePath + "/mobiiti_technique/trips/",organisationName.toUpperCase());
+        ZipFileUtils.deleteDirectory(organisationDirectory);
+    }
+
+    /**
      * Get the destination directory to store stop_times.txt files
      * @param organisation
      *  the organisation name
      * @return
      *  the directory in which the file will be stored
      */
-    private String getDestinationDir(String organisation){
-        return chouetteStoragePath + "/mobiiti_technique/stop_times/" + organisation.toUpperCase();
+    private String getDestinationDir(String organisation, String fileType){
+        return chouetteStoragePath + "/mobiiti_technique/" + fileType + "/" + organisation.toUpperCase();
     }
 
     /**
@@ -79,10 +119,10 @@ public class StopTimesArchiver {
      * @return
      *  the file names
      */
-    private String getDestinationFileName(){
+    private String getDestinationFileName(String prefix){
         LocalDateTime currDate = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddhhmmss");
         String dateStr = currDate.format(formatter);
-        return "stop_times_" + dateStr + ".txt";
+        return prefix + dateStr + ".txt";
     }
 }
