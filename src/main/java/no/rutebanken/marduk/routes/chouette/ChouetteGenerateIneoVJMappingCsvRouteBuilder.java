1997eb1d069d3f51163e6aa5c14d2547f35a727f
afreche <alexandre.freche@lumiplan.com>
2024-10-28 19:05:40 +0100

    DATA-574: add CRON to generate vehicleJourneyMapping.csv from chouette

---
+++ src/main/java/no/rutebanken/marduk/routes/chouette/ChouetteGenerateIneoVJMappingCsvRouteBuilder.java
@@ -0,0 +1,47 @@
+package no.rutebanken.marduk.routes.chouette;
+
+import no.rutebanken.marduk.Constants;
+import no.rutebanken.marduk.routes.BaseRouteBuilder;
+import org.apache.camel.Exchange;
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.component.http4.HttpMethods;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ChouetteGenerateIneoVJMappingCsvRouteBuilder extends BaseRouteBuilder {
+
+    @Value("${chouette.generateVjMappingCsv.schedule:0+00+04+?+*+MON-FRI}")
+    private String cronSchedule;
+
+    @Value("${chouette.url}")
+    private String chouetteUrl;
+
+    @Value("${chouette.generateVjMappingCsv:false}")
+    private boolean enabled;
+
+    @Override
+    public void configure() throws Exception {
+        if (!enabled) {
+            return;
+        }
+
+        super.configure();
+
+        singletonFrom("quartz2://marduk/chouetteGenerateIneoVJMappingCsvQuartz?cron=" + cronSchedule + "&trigger" +
+                ".timeZone=" + Constants.TIME_ZONE)
+                .filter(e -> shouldQuartzRouteTrigger(e, cronSchedule))
+                .log(LoggingLevel.INFO, "Quartz triggers Chouette GenerateIneoVJMappingCsv")
+                .to("direct:chouetteGenerateIneoVJMappingCsv")
+                .routeId("chouette-GenerateIneoVJMappingCsv-quartz");
+
+        from("direct:chouetteGenerateIneoVJMappingCsv")
+                .log(LoggingLevel.INFO, correlation() + "Starting Chouette GenerateIneoVJMappingCsv")
+                .removeHeaders("Camel*")
+                .setBody(constant(null))
+                .setHeader(Exchange.HTTP_METHOD, constant(HttpMethods.GET))
+                .toD(chouetteUrl + "/chouette_iev/admin/generate_ineo_vj_mapping")
+                .log(LoggingLevel.INFO, correlation() + "Completed Chouette GenerateIneoVJMappingCsv")
+                .routeId("chouette-GenerateIneoVJMappingCsv");
+    }
+}
