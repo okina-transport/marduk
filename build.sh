2ad66bda6bf73087a87c9db329680ed95fbed784
afreche <alexandre.freche@lumiplan.com>
2025-01-27 10:50:55 +0100

    fix: update project version retrieval

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ mvn clean package -DskipTests
 MVN_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2};?)?)?[mGK]//g")
 IMAGE_NAME=registry.okina.fr/mobiiti/marduk:"${MVN_VERSION}"
 
-docker build -t "${IMAGE_NAME}" --build-arg JAR_FILE=target/marduk-"${VERSION_BACK}".jar .
+docker build -t "${IMAGE_NAME}" --build-arg JAR_FILE=target/marduk-"${MVN_VERSION}".jar .
 docker push "${IMAGE_NAME}"
