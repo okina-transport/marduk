9829c58b6cf04979710651ff22cb5978d21cb5d9
jmarzin1 <julien.marzin@lumiplan.com>
2026-04-02 10:56:56 +0200

    fix() : avoid double file read and copy of same file

--- src/main/java/no/rutebanken/marduk/services/processors/FileValidationProcessor.java
+++ src/main/java/no/rutebanken/marduk/services/processors/FileValidationProcessor.java
@@ -5,6 +5,7 @@ import no.rutebanken.marduk.routes.file.ZipFileUtils;
 import no.rutebanken.marduk.utils.FileUtils;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.apache.commons.lang3.Strings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -13,6 +14,7 @@ import java.nio.file.Path;
 import java.util.Set;
 
 import static no.rutebanken.marduk.Constants.CLEAN_INPUT_NETEX_ZIP;
+import static no.rutebanken.marduk.Constants.INPUT_OFFER_ZIP_FILE_PATH;
 
 @Component
 public class FileValidationProcessor implements Processor {
@@ -28,11 +30,12 @@ public class FileValidationProcessor implements Processor {
         if (invalidZipInput) {
             exchange.getIn().setHeader(CLEAN_INPUT_NETEX_ZIP, Boolean.TRUE);
         }
+        exchange.getIn().setHeader(INPUT_OFFER_ZIP_FILE_PATH, path);
     }
 
     private boolean isZipContainingInvalidFiles(Set<String> files, String importType ) {
         boolean isInvalid = false;
-        if ("gtfs".equals(importType)) {
+        if (Strings.CI.equals("gtfs",importType)) {
             for (String file : files) {
                 if (!file.endsWith(".txt")) {
                     String errorMsg = "GTFS file containing non-txt file:" + file;
